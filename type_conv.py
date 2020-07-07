502d677d34c36a79c1ef52e98938f437a10fd1e7
pvineeta <vineeta.pandey30@gmail.com>
2020-07-07 10:38:56 +0100

    one more file to update on remote

---
+++ type_conv.py
@@ -0,0 +1,5 @@
+#write a program to convert the data type of float number to an integer number
+
+fl = 1.8 # assign a float value to this variable
+integerNum = int(fl) # now change data type using int() method
+print(integerNum)
