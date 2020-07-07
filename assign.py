df112abecb58ca2abf1fbbbd5eebd0fb1cf39682
pvineeta <vineeta.pandey30@gmail.com>
2020-07-07 16:59:13 +0100

    upload

---
+++ assign.py
@@ -0,0 +1,6 @@
+#write a program to assign values to two variables using assignment and after that add 1 to both variables.
+
+x,y = 10, 20            # assign values x=10 and y=20 using comman
+x,y =  x+1, y+1           # now add 1 value to both variable and assign using comman
+
+print(x, y)       # now print value of x and y;
